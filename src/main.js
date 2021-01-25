9ce1de70d6ec3806111ec88ef20b80e16fbd1b36
ebuonauro <contact@emilyalleen.com>
2021-01-24 18:59:47 -0800

    Added bootstrap and bootstrap typeahead. About to pull in Nuxt

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueTypeaheadBootstrap from 'vue-typeahead-bootstrap';
+
+// Bootstrap and Bootstrap Vue
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make Bootstrap globally available throughout the project
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
+
+Vue.component('vue-typeahead-bootstrap', VueTypeaheadBootstrap)
 
 new Vue({
   router,
